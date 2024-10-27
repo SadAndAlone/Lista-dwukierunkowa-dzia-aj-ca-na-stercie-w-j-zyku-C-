9803a80ff5c93b0e3c116cd50d723a44596296ab
TrudovMykhailo <156135082+SadAndAlone@users.noreply.github.com>
2024-10-27 17:21:59 +0100

    dodawanie klasy DubleLinkedList

--- Lista dwukierunkowa działająca na stercie w języku C++/Lista dwukierunkowa działająca na stercie w języku C++.cpp
+++ Lista dwukierunkowa działająca na stercie w języku C++/Lista dwukierunkowa działająca na stercie w języku C++.cpp
@@ -1,13 +1,19 @@
 ﻿#include <iostream>
-#include <cstdlib> // dla rand() i srand()
-#include <ctime>   // dla time()
+#include <cstdlib> //#include <cstdlib> - dla rand() i srand()
+#include <ctime>   //#include <ctime> - dla time()
 using namespace std;
 
 struct Node {   //Node bedzie reprezentować pojedynczy element w liscie dwukierunkowej. Składa się z trzech częsci:
-    int data;   //data — przechowuje wartość 
+    int data;   //data - przechowuje wartość 
     Node* prev; //prev — wskaznik na poprzedni element listy
     Node* next; //next — wskaznik na następny element w liście
     Node(int value) : data(value), prev(nullptr), next(nullptr) {} //Node(int value) ustawia wartosc data na value, 
     //a wskaźniki prev(nullptr) i next(nullptr) na nullptr co oznacza że poczatkowo element nie jest połączony z innymi elementami
 };
+
+class DubleLinkedList { //tworzymy class dla listy
+private: //w private będą przechowywane head (poczatek listy) i back (koniec listy)
+    Node* head; //head - poczatek listy
+    Node* back; //back - koniec listy
+
 
